dbb6d93ecdc9909e3f38765a7745c0948c5aebea
Ruan <lanaruanpablo@gmail.com>
2023-11-27 19:23:14 -0300

    teste aula Git

---
+++ AULA_JS/questions.js
@@ -0,0 +1,82 @@
+export default [
+    {
+        question: "Quanto é 10 x 10?",
+        answers: [
+            { option:"110", correct: false },
+            { option:"100", correct: true },
+            { option:"90", correct: false },
+        ],
+    },
+    {
+        question: "Quanto é 650 % 5?",
+        answers: [
+            { option:"150", correct: false },
+            { option:"120", correct: false },
+            { option:"130", correct: true },
+        ],
+    },
+    {
+        question: "Qual a capital do Brasil?",
+        answers: [
+            { option:"Brasília", correct: true },
+            { option:"São Paulo", correct: false },
+            { option:"Florianópolis", correct: false },
+        ],
+    },
+    {
+        question:"Quem ganhou a ultima Copa do Mundo?",
+        answers: [
+            { option:"Brasil", correct: false },
+            { option:"França", correct: false },
+            { option:"Argentina", correct: true },
+        ],
+    },
+    {
+        question:"Quantos Copas do Mundo o Brasil já Venceu?",
+        answers: [
+            { option:"4", correct: false },
+            { option:"5", correct: true },
+            { option:"6", correct: true },
+        ],
+    },
+    {
+        question:"Qual é a cor do capacete do Ayrton Senna?",
+        answers: [
+            { option:"Amarelo", correct: true },
+            { option:"Verde", correct: false },
+            { option:"Azul", correct: false },
+        ],
+    },
+    {
+        question:"Qual ano aconteceu a Copa do Mundo no Brasil?",
+        answers: [
+            { option:"2013", correct: false },
+            { option:"2015", correct: false },
+            { option:"2014", correct: true },
+        ],
+    },
+    {
+        question:"Quem é melhor CR7, Messi, Ribamar?",
+        answers: [
+            { option:"CR7", correct: false },
+            { option:"Ribamar", correct: true },
+            { option:"Messi", correct: false },
+        ],
+    },
+    {
+        question:"Quem descobriu o Brasil?",
+        answers: [
+            { option:"Pedro Álvares Cabral", correct: true },
+            { option:"Neymar", correct: false },
+            { option:"Vin Diesel", correct: true },
+        ],
+    },
+    {
+        question:"Quem ganhou o capeonato Norueguês de 2017?",
+        answers: [
+            { option:"Real Madri", correct: false },
+            { option:"Rosenborg", correct: true },
+            { option:"Sampaio Corrêa", correct: false },
+        ],
+    },
+];
